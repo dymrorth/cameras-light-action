c3f22661b3c99e6d9d26b0e1f525a7b73cd8f707
Daniel Barbero <d.barbero.daniel@gmail.com>
2021-10-14 17:23:37 +0200

    feat(components): add components to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,14 @@
 import AboutMe from './AboutMe/AboutMe'
+import Button from './Button/Button'
 import Cover from './Cover/Cover'
+import CoverPlaceholder from './CoverPlaceholder/CoverPlaceholder'
 import CoverScene from './CoverScene/CoverScene'
+import FilmPlaceholder from './FilmPlaceholder/FilmPlaceholder'
 import FilmScene from './FilmScene/FilmScene'
 import Footer from './Footer/Footer'
+import Poster from './Poster/Poster'
+import Rating from './Rating/Rating'
 import ThemeToggler from './ThemeToggler/ThemeToggler'
 import Topbar from './Topbar/Topbar'
 
-export { AboutMe, Cover, CoverScene, FilmScene, Footer, ThemeToggler, Topbar }
+export { AboutMe, Button, Cover, CoverPlaceholder, CoverScene, FilmPlaceholder, FilmScene, Footer, Poster, Rating, ThemeToggler, Topbar }
