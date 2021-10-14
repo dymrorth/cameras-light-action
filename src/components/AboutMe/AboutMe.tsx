5ecf5805b50b5122eca722ee8a5f815a4099818b
Daniel Barbero <d.barbero.daniel@gmail.com>
2021-10-15 01:35:57 +0200

    feat(components): wip About me

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
-const AboutMe = () => {
+import { EMAIL } from 'constants/index'
+
+import styles from './AboutMe.module.scss'
+
+const AboutMe: React.FC<{}> = () => {
     return (
-        <div>
-            About Me
-        </div>
+        <section className={styles.card}>
+            <div className={styles.front}>
+                <h3 className={styles.title}>Daniel Barbero Muñoz</h3>
+                <p className={styles.subtitle}>Frontend Developer</p>
+            </div>
+
+            <div className={styles.back}>
+                <p className={styles.body}>
+                    Hello! My name is Daniel and I'm a frontend developer. <br />
+                    I create web apps, I'm interested in data visualization and I like learning new tech.
+                    <br />
+                    <br /> Always trying to make a better world.
+                    <br />
+                    <br />
+                    <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
+                </p>
+            </div>
+        </section>
     )
 }
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
 export { ROUTES } from './routes'
 export const LOCAL_USER_THEME_KEY = 'user-theme'
+export const EMAIL = 'd.barbero.daniel@gmail.com'
