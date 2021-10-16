46363b06a3757a5ba7e674b06f06d9771940504d
danielb <d.barbero.daniel@gmail.com>
2021-10-16 03:58:50 +0200

    feat(components): add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Footer = () => {
+import styles from './Footer.module.scss'
+
+import { ROUTES, LINKEDIN_PROFILE_URL, GITHUB_PROFILE_URL } from 'constants/index'
+
+import { Link } from 'react-router-dom'
+
+const Footer: React.FC<{}> = () => {
     return (
-        <footer>
-            Footer
+        <footer className={styles.footer}>
+            <div className={styles.content}>
+                <Link to={ROUTES.home}>Home</Link>
+                <Link to={ROUTES.aboutMe}>About Me</Link>
+                <a href={LINKEDIN_PROFILE_URL} target="_blank">
+                    Linkedin
+                </a>
+                <a href={GITHUB_PROFILE_URL} target="_blank">
+                    Github
+                </a>
+            </div>
         </footer>
     )
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export { ROUTES } from './routes'
+export * from './personalData'
+
 export const LOCAL_USER_THEME_KEY = 'user-theme'
-export const EMAIL = 'd.barbero.daniel@gmail.com'
