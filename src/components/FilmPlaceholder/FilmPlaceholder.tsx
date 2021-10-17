c4e7dd46faca2043323d702f00e4e4416309ec66
danielb <d.barbero.daniel@gmail.com>
2021-10-17 12:57:51 +0200

    feat(film placeholder): add styling

--- src/components/FilmPlaceholder/FilmPlaceholder.tsx
+++ src/components/FilmPlaceholder/FilmPlaceholder.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import styles from './FilmPlaceholder.module.scss'
+
 const FilmPlaceholder = () => {
     return (
-        <div>
-            loading ...
+        <div className={styles.film}>
+            <div className={styles.poster}></div>
+            <div className={styles.content}>
+                <div className={styles.top}>
+                    <div className={styles.title}></div>
+                    <div className={styles.rating}></div>
+                </div>
+                <div className={styles.overview}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
         </div>
     )
 }
